5123cf1a9a12a6fea82f8ef55ea9b9bfd84fb2aa
Will Tabor <tabor.will@gmail.com>
2016-05-16 15:12:09 -0400

    update user route

--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,76 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../models/user');
+
 // GET users
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-// SHOW
-router.get('/:id'), function(req, res ,next) {
-  res.
+//=================================================
+//AUTHENTICATION
+//=================================================
+function authenticate(req, res, next) {
+    if (!req.isAuthenticated()) {
+        res.redirect('/');
+    } else {
+        next();
+    }
 }
 
+// INDEX
+router.get('/', authenticate, function(req, res, next) {
+    var user = global.currentUser.users;
+    res.render('users/index', { users: users, message: req.flash() });
+});
+
+
+// SHOW
+router.get('/:id', authenticate, function(req, res, next) {
+    var user = currentUser.users.id(req.params.id);
+    if (!user) return next(makeError(res, 'Document not found', 404));
+    res.render('users/show', { user: user, message: req.flash() });
+});
+
 // EDIT
+router.get('/:id/edit', authenticate, function(req, res, next) {
+    var user = currentUser.users.id(req.params.id);
+    if (!user) return next(makeError(res, 'Document not found', 404));
+    res.render('user/edit', { user: user, message: req.flash() });
+});
 
 // UPDATE
+router.put('/:id', authenticate, function(req, res, next) {
+    var user = currentUser.users.id(req.params.id);
+    if (!user) return next(makeError(res, 'Document not found', 404));
+    else {
+        user.userTheme = req.body.userTheme;
+        currentUser.save()
+            .then(function(saved) {
+                res.redirect('/users');
+            }, function(err) {
+                return next(err);
+            });
+    }
+});
 
-// DELETE
-
-
-
-
+// DESTROY
+router.delete('/:id', authenticate, function(req, res, next) {
+    var user = currentUser.users.id(req.params.id);
+    if (!user) return next(makeError(res, 'Document not found', 404));
+    var index = currentUser.users.indexOf(user);
+    currentUser.users.splice(index, 1);
+    currentUser.save()
+        .then(function(saved) {
+            res.redirect('/users');
+        }, function(err) {
+            return next(err);
+        });
+});
 
 module.exports = router;
+
 
 
 
