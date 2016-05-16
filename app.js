dce609b32e5ceb470a267a26bf8c8bda2d137108
Will Tabor <tabor.will@gmail.com>
2016-05-16 09:51:03 -0400

    started passport

--- app.js
+++ app.js
@@ -1,26 +1,62 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+//=================================================
+//GLOBAL
+//=================================================
+var express        = require('express');
+var path           = require('path');
+var favicon        = require('serve-favicon');
+var logger         = require('morgan');
+var cookieParser   = require('cookie-parser');
+var bodyParser     = require('body-parser');
+var passport       = require('passport');
+var mongoose       = require('mongoose');
+var methodOverride = require('method-override');
+var session        = require('express-session');
+var flash          = require('connect-flash');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+//=================================================
+//ROUTES
+//=================================================
+var homeRouter    = require('./routes/index');
+var usersRouter   = require('./routes/users');
+var promptsRouter = require('./routes/prompts');
+var storiesRouter = require('./routes/stories');
 
 var app = express();
 
-// view engine setup
+//=================================================
+//CONNECT TO DATABASE
+//=================================================
+
+
+//=================================================
+//VIEW ENGINE SETUP
+//=================================================
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+//=================================================
+//USE SECTION
+//=================================================
+app.use(logger('combined')); //possibly switch to 'dev'
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+//=================================================
+//PASSPORT SESSION
+//=================================================
+app.use(session({ secret: 'WDI Rocks!',
+                  resave: true,
+                  saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('./config/passport/passport')(passport);
 
 app.use('/', routes);
 app.use('/users', users);
